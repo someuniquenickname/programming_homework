b1da32ea5a7d392eba0913455759f38257ab59cd
unnamed <aghast-oil-grunt@duck.com>
2025-04-27 02:01:06 +0300

    Create task06.py

---
+++ part_1/task06.py
@@ -0,0 +1,16 @@
+a = input("Введите число а -->")
+
+first = a
+first_pow_2 = first * first  # 1
+first_pow_4 = first_pow_2 * first_pow_2 # 2
+
+second = a
+second_pow_2 = second * second # 1
+second_pow_4 = second_pow_2 * second_pow_2 # 2
+second_pow_6 = second_pow_2 * second_pow_4 # 3
+
+third = a
+third_pow_3 = a * a * a # 2
+third_pow_6 = third_pow_3 * third_pow_3 # 3
+third_pow_12 = third_pow_6 * third_pow_6 # 4
+third_pow_15 = third_pow_12 * third_pow_3 # 5
