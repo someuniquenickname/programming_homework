685b7ac7a13e0d0fb8912f4ee8487b8b96b0baaf
unnamed <aghast-oil-grunt@duck.com>
2025-05-18 15:36:19 +0300

    Create task09.py

---
+++ part_2/task09.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        line = input("-->")
+        result = len(line.split())
+        break
+    except:
+        print("Невалидная строка")
+print(f"Кол-во слов в этой строке равно {result}")
