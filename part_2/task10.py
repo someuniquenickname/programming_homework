e8b6aa2183befb444235cd92e8ab7d4914837525
doroty <example.com>
2025-06-11 20:59:50 +0300

    ...

--- part_2/task10.py
+++
@@ -1,6 +0,0 @@
-import re
-
-string = str(input("-->"))
-for i in string:
-    
-print(len(res))
--- part_3/task03.py
+++ part_3/task03.py
@@ -2,7 +2,7 @@ with open("part_3/files/fio.txt", "r") as file:
     tmp = [ name.split() for name in file.read().lower().strip().split("\n")]
 
 
-#quick search
+
 
 
 with open("part_3/files/fio2.txt", "w") as file2:
